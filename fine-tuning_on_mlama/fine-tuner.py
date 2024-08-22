6a943781ee5eec7b893b84c81e8098770492a695
vivalavida <barid.x.ai@gmail.com>
2024-08-23 01:41:48 +0800

    Update fine-tuner.py

--- fine-tuning_on_mlama/fine-tuner.py
+++ fine-tuning_on_mlama/fine-tuner.py
@@ -27,11 +27,10 @@ def tokenize_wiki_examples(examples, tokenizer):
     return tokenizer(examples["text"], padding=True,  truncation=True)
 def load_training_validation_dataset(tokenizer):
     #  mlama 53 is sorted in order of statements.
-    df = pd.read_parquet("./mlama53.parquet", engine="fastparquet")
-    m_lama = Dataset.from_pandas(df)
+    m_lama = load_dataset("parquet", data_files="./mlama53.parquet")
     val_dataset = load_dataset("wikitext", "wikitext-2-raw-v1", split="test")
-    tokenized_train = m_lama.map(lambda examples: tokenize_mlama_examples(examples, tokenizer), batched=False,remove_columns=df.columns.values.tolist())
-    tokenized_val = val_dataset.map(lambda examples: tokenize_wiki_examples(examples, tokenizer), batched=True,remove_columns=["text"])
+    tokenized_train = m_lama.map(lambda examples: tokenize_mlama_examples(examples, tokenizer), batched=False,remove_columns=m_lama.column_names)
+    tokenized_val = val_dataset.map(lambda examples: tokenize_wiki_examples(examples, tokenizer), batched=True,remove_columns=val_dataset.column_names)
 
     return tokenized_train, tokenized_val
 
