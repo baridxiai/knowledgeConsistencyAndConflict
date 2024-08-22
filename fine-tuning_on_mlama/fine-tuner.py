e223e5e343cd37dc9d824d9f4435aac9814c4463
vivalavida <barid.x.ai@gmail.com>
2024-08-23 01:22:20 +0800

    debug

--- fine-tuning_on_mlama/fine-tuner.py
+++ fine-tuning_on_mlama/fine-tuner.py
@@ -12,7 +12,7 @@ from datasets import Dataset,load_dataset
 import pandas as pd
 import torch
 import datetime
-torch.distributed.init_process_group(backend="nccl", timeout=datetime.timedelta(days=1))
+#torch.distributed.init_process_group(backend="nccl", timeout=datetime.timedelta(days=1))
 
 # Training
 
@@ -22,15 +22,15 @@ def tokenize_mlama_examples(examples, tokenizer):
     template = examples["template"]
     examples = template.replace("[X]", sub_label).replace("[Y]", obj_label)
 
-    return tokenizer(examples, padding="max_length")
+    return tokenizer(examples, padding=True,  truncation=True)
 def tokenize_wiki_examples(examples, tokenizer):
-    return tokenizer(examples["text"], padding="max_length")
+    return tokenizer(examples["text"], padding=True,  truncation=True)
 def load_training_validation_dataset(tokenizer):
     #  mlama 53 is sorted in order of statements.
     df = pd.read_parquet("./mlama53.parquet", engine="fastparquet")
     m_lama = Dataset.from_pandas(df)
     val_dataset = load_dataset("wikitext", "wikitext-2-raw-v1", split="test")
-    tokenized_train = m_lama.map(lambda examples: tokenize_mlama_examples(examples, tokenizer), batched=False)
+    tokenized_train = m_lama.map(lambda examples: tokenize_mlama_examples(examples, tokenizer), batched=False,remove_columns=df.columns.values).batch(batch_size=53)
     tokenized_val = val_dataset.map(lambda examples: tokenize_wiki_examples(examples, tokenizer), batched=True,remove_columns=["text"])
 
     return tokenized_train, tokenized_val
