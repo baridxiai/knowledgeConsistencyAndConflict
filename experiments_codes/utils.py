14298b79a6f726bb5dc0501ddd2a7be3282f8a51
vivalavida <barid.x.ai@gmail.com>
2025-02-15 17:08:18 +0800

    major

--- experiments_codes/utils.py
+++ experiments_codes/utils.py
@@ -30,6 +30,7 @@ def initialize_wrapped_model_and_tokenizer(model_name:str, task_type:str, use_cu
             tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
             model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16, # force fp16 mode
             device_map="auto")
+            model = LlamaHelper(model,tokenizer)
         wrapped_model = DecoderWrapper(model, tokenizer,task_type)
     else: # encoder
         if not use_custom_bias_model:
